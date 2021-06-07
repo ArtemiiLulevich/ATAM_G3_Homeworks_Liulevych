9ee8032659446468bdedabc277cf2ba8faefc09b
artem <artemii.lulevich@gmail.com>
2021-06-07 12:18:25 +0300

    Second homework done.

--- src/Homework_1/Application.java
+++ src/Homework_1/Application.java
@@ -1,11 +1,25 @@
 package Homework_1;
 
+
+import javax.management.relation.RoleList;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
 
         DataClass dataClass = new DataClass();
+        List<String> convertedList = dataClass.arrayToListConverter(dataClass.anArrayOfCities);
 
-        SubroutineClass.printList(dataClass.arrayToListConverter(dataClass.anArrayOfCities));
+        //Homework_1
+        System.out.println("=============Homework_1=============");
+        SubroutineClass.printList(convertedList);
 
+        //Homework_2
+        System.out.println("=============Homework_2=============");
+        SubroutineClass.streamList(null);
+        SubroutineClass.streamList(convertedList);
+        SubroutineClass.streamList(new ArrayList<String>());
     }
+
 }
--- src/Homework_1/SubroutineClass.java
+++ src/Homework_1/SubroutineClass.java
@@ -1,5 +1,6 @@
 package Homework_1;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class SubroutineClass {
@@ -7,11 +8,29 @@ public class SubroutineClass {
      *  Класс созданый по второму пункту задания.
      *
      *  printList - метод выводящий в консоль данные из списка.
+     *
+     *  streamList - метод выводящий в консоль данные в формате массива букв.
+     *  Так же, обрабатывает исключения.
      */
 
     public static void printList(List<String> anyList){
         for (String someData: anyList){
             System.out.println(someData);
         }
+    }
+
+    public static void streamList(List<String> anyList){
+        try {
+            anyList.stream().
+                    map(element -> Arrays.toString(element.split(""))).
+                    forEach(System.out::println);
+        } catch (NullPointerException e){
+            System.out.println("The array is undefined. Please check " +
+                    "if the entered values are correct.");
+        } catch (Exception e){
+            System.out.println("An unknown error has occurred. " +
+                    "More details: " + e.getMessage());
+        }
+
     }
 }
